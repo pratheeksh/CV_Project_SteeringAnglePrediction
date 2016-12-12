b087c0e958874bbd6cdd9be087d7502d00e3dd95
pratheeksh <pks329@nyu.edu>
2016-12-12 12:43:35 -0500

    evaluation code ready

--- evaluate.py
+++ evaluate.py
@@ -1,23 +1,24 @@
 import sys
 import pickle
 import math
-results = open(sys.argv[1])
+import csv
+results = csv.reader(open(sys.argv[1]))
 
 evaluation_data = {}
 try:
 	evaluation_data = pickle.load(open("eval.p", "rb"))
 except:
-	eval_csv = open("eval.csv",r)
-	for l in eval_csv.readlines():
-		frame_id, angle = eval_csv.split(',')
+	eval_csv = csv.reader(open("eval.csv"))
+	for l in eval_csv:
+		frame_id, angle = l[0], l[1]
 		if frame_id == "frame_id":
 			continue
 		evaluation_data[frame_id] = float(angle)
-		pickle.dump(evaluation_data, open("eval.p", wb))
+		pickle.dump(evaluation_data, open("eval.p", "wb"))
 error = 0
 n = 0
-for l in results.readlines():
-	frame_id, angle = results.split(',')
+for l in results:
+	frame_id, angle = l[0], l[1]
 	if frame_id == "frame_id":
 		continue
 	error += (evaluation_data[frame_id] - float(angle)) * (evaluation_data[frame_id] - float(angle))
