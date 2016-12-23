d5ae31cd724b5132affcf04424e057143a14c1d5
pratheeksh <pks329@nyu.edu>
2016-12-23 17:56:45 -0500

    changed plotter - can plot multple csvs now

--- plotter.py
+++ plotter.py
@@ -1,13 +1,30 @@
-import matplotlib.pyplot as plt
-import csv
+import pickle
 import sys
 
+import matplotlib.pyplot as plt
+
 plots = []
+evaluation_data = pickle.load(open("eval.p", "rb"))
+
 x = range(5614)
+i = 1
+k = len(sys.argv[1:])
+
 for filename in sys.argv[1:]:
+    original_angles = []
+
+    ax = plt.subplot(k, 1, i)
+    i += 1
     dummy = []
-    for row in open(filename,'rb'):
-        dummy.append(float(row))
-    plt.plot(x,dummy,label=filename.split('.')[0])
+    for row in open(filename, 'rb'):
+        if "Filename" in row:
+            continue
+
+        frame_id, angle = row.split(',')
+        original_angles.append(evaluation_data[frame_id])
+        dummy.append(float(angle))
+    ax.plot(x, dummy, label="Predicted Angles")
+    ax.scatter(x, original_angles, label="Original Angles", color='green', s=1)
+
 plt.legend()
 plt.show()
